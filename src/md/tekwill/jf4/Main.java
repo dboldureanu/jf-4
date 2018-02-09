0d4746cdaacdc42e7d0bdf08b46bc4911eea6166
sskknn <ss_kk_nn@mail.ru>
2018-02-09 20:25:47 +0200

    set line #16_serioga peshakoff

--- src/md/tekwill/jf4/Main.java
+++ src/md/tekwill/jf4/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("13");
         System.out.println("14");
         System.out.println("15");
-        System.out.println("16");
+        System.out.println("Serioga Peshakoff");
         System.out.println("17");
         System.out.println("18");
         System.out.println("19");
