ad7baf74d22002acf34f2db199c4c424e3948310
Dumitru Boldureanu <calendox.backup@gmail.com>
2018-02-09 07:18:44 +0200

    init

---
+++ src/md/tekwill/jf4/Main.java
@@ -0,0 +1,29 @@
+package md.tekwill.jf4;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Each of you got a number. Make sure that line number prints your name. Update that line only!
+        System.out.println("7");
+        System.out.println("8");
+        System.out.println("9");
+        System.out.println("10");
+        System.out.println("11");
+        System.out.println("12");
+        System.out.println("13");
+        System.out.println("14");
+        System.out.println("15");
+        System.out.println("16");
+        System.out.println("17");
+        System.out.println("18");
+        System.out.println("19");
+        System.out.println("20");
+        System.out.println("21");
+        System.out.println("22");
+        System.out.println("23");
+        System.out.println("24");
+        System.out.println("25");
+        System.out.println("26");
+        System.out.println("27");
+    }
+}
