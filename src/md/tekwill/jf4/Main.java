00c38f7bcd775e8a60920881f31357356650d73a
alex <test>
2018-02-09 20:08:33 +0200

    Set output on line 12 to Alexei

--- src/md/tekwill/jf4/Main.java
+++ src/md/tekwill/jf4/Main.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.println("9");
         System.out.println("10");
         System.out.println("11");
-        System.out.println("12");
+        System.out.println("Alexei");
         System.out.println("13");
         System.out.println("14");
         System.out.println("15");
