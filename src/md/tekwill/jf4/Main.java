7ad61bb84566b3f74124ea5839e188c3966ff5bc
Vitalicus <lisnic.v@gmail.com>
2018-02-09 20:08:14 +0200

    Set Lisnic Vitalie on 17 line

--- src/md/tekwill/jf4/Main.java
+++ src/md/tekwill/jf4/Main.java
@@ -14,7 +14,7 @@ public class Main {
         System.out.println("14");
         System.out.println("15");
         System.out.println("16");
-        System.out.println("17");
+        System.out.println("Vitalie Lisnic");
         System.out.println("18");
         System.out.println("19");
         System.out.println("20");
