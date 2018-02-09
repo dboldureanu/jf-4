02b603e155ce4019a5b14973db84d2ec961d6bf1
Victor Luca <Victor@Pavilion-G6.office.lastrada.md>
2018-02-09 20:08:04 +0200

    Set Victor Luca on line 15

--- src/md/tekwill/jf4/Main.java
+++ src/md/tekwill/jf4/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println("12");
         System.out.println("13");
         System.out.println("14");
-        System.out.println("15");
+        System.out.println("Victor Luca");
         System.out.println("16");
         System.out.println("17");
         System.out.println("18");
