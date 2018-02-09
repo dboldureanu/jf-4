b4a8857fd171b7e04514a4c45761677c65ddb3db
dboldureanu <dboldureanu@gmail.com>
2018-02-09 20:40:21 +0200

    clear

--- src/md/tekwill/jf4/Main.java
+++ src/md/tekwill/jf4/Main.java
@@ -4,29 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	    // Each of you got a number. Make sure that line number prints your name. Update that line only!
-<<<<<<< HEAD
         System.out.println("7");
         System.out.println("8");
         System.out.println("9");
-=======
-        System.out.println("Darii Olea");
-        System.out.println("Prutean Jack");
-        System.out.println("Jana");
->>>>>>> 967a1f59f6e567a5e70ec914544ac1088120d348
         System.out.println("10");
-        System.out.println("Andrei Cristea");
+        System.out.println("11");
         System.out.println("12");
         System.out.println("13");
         System.out.println("14");
-        System.out.println("Victor Luca");
-        System.out.println("Serioga Peshakoff");
-        System.out.println("Vitalie Lisnic");
+        System.out.println("15");
+        System.out.println("16");
+        System.out.println("17");
         System.out.println("18");
         System.out.println("19");
         System.out.println("20");
         System.out.println("21");
         System.out.println("22");
-        System.out.println("Dumitru Boldureanu");
+        System.out.println("23");
         System.out.println("24");
         System.out.println("25");
     }
