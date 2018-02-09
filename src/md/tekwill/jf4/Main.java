e6603b55c1e9abcf924c05e66ad2c5237e8b4f2a
Sergiu.Cretu <32588214+shyene27@users.noreply.github.com>
2018-02-09 20:09:00 +0200

    set sergiu on line 13

--- src/md/tekwill/jf4/Main.java
+++ src/md/tekwill/jf4/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("10");
         System.out.println("11");
         System.out.println("12");
-        System.out.println("13");
+        System.out.println("Sergiu");
         System.out.println("14");
         System.out.println("15");
         System.out.println("16");
