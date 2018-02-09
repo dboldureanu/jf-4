d43169785a96d08f92a0e91ad81ec95f47f7b77c
AVK <recran80@gmail.com>
2018-02-09 20:11:06 +0200

    set Andrei Cristea on line 11

--- src/md/tekwill/jf4/Main.java
+++ src/md/tekwill/jf4/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("8");
         System.out.println("9");
         System.out.println("10");
-        System.out.println("11");
+        System.out.println("Andrei Cristea");
         System.out.println("12");
         System.out.println("13");
         System.out.println("14");
